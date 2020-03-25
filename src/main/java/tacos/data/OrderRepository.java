28468c1da66b20fb3be7702cf1ca32cbf7fc397a
baardcj <baardcj@outlook.com>
2020-03-25 18:51:07 +0100

    New JPA version

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -2,8 +2,29 @@ package tacos.data;
 
 import tacos.Order;
 
-public interface OrderRepository {
+import java.sql.Date; // may be from java util
+import java.util.List;
 
-  Order save(Order order);
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+	
+	
+	/*
+	 * Customizing JPA repositories 
+	 * Examples of queries by domain specific language (DSL) of JPA
+	 */
+	/*
+	List<Order> findByDeliveryZip(String deliveryZip);
+	
+	List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+	
+	List<Order> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
+	
+	List<Order> findByDeliveryCityOrderByDeliveryTo(String city);
+	
+	@Query("Order o where o.deliveryCity='Seattle'")
+	List<Order> readOrdersDeliveredInSeattle(); */ 
 }
--- src/main/java/tacos/web/IngredientByIdConverter.java
+++ src/main/java/tacos/web/IngredientByIdConverter.java
@@ -1,5 +1,7 @@
 package tacos.web;
 
+import java.util.Optional; 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -17,9 +19,18 @@ public class IngredientByIdConverter implements Converter<String, Ingredient> {
     this.ingredientRepo = ingredientRepo;
   }
   
+  /*
   @Override
   public Ingredient convert(String id) {
     return ingredientRepo.findOne(id);
+  }
+  */
+  
+  @Override
+  public Ingredient convert(String id) {
+    Optional<Ingredient> optionalIngredient = ingredientRepo.findById(id);
+	return optionalIngredient.isPresent() ?
+		   optionalIngredient.get() : null;
   }
 
 }
